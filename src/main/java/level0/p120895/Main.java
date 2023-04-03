a2bf444cb1daad8346cc485ab5ebda5674218c55
jny0 <pjy100402@naver.com>
2023-04-03 09:55:03 +0900

    풀이

---
+++ src/main/java/level0/p120895/Main.java
@@ -0,0 +1,18 @@
+package level0.p120895;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "hello";
+        int num1 = 1;
+        int num2 = 2;
+        System.out.println(new Solution().solution(str, num1, num2));
+    }
+}
+
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+
+        return answer;
+    }
+}
